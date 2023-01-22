fd3b787742ec4e9a2bc898e94e7d3d7fed315706
Ylerson Almonte <yacwarefree1@gmail.com>
2023-01-22 13:12:24 -0400

    Delete funcion counter

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 import './style.css';
-import {App} from './src/app';
+import App from './src/app';
 
 document.querySelector('#app').innerHTML = App();
-
-setupCounter(document.querySelector('#counter'));
