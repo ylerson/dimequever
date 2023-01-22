5401a6bbf91d3d7a099b4fb6323325dc727f379a
Ylerson Almonte <yacwarefree1@gmail.com>
2023-01-22 17:32:15 -0400

    set background placeholder

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,12 +4,14 @@ import MovieMedia from './MovieMedia';
 export default function Header(movieImage, movieDetail, movieCast) {
     const imagenBackground = movieImage?.backdrops[0];
     if (imagenBackground?.file_path === undefined) return location.reload();
-
-    const urlImagen = `https://image.tmdb.org/t/p/original/${imagenBackground?.file_path}`;
+    const setBackground =
+        imagenBackground?.file_path === undefined
+            ? './dark-wood.jpg'
+            : `https://image.tmdb.org/t/p/original/${imagenBackground?.file_path}`;
 
     return `
     <div class="w-full h-full fixed top-0 left-0">
-        <img class="w-full h-full object-cover " src=${urlImagen}></img>
+        <img class="w-full h-full object-cover " src=${setBackground}></img>
         <div class="absolute w-full h-96 bottom-0 from-gray-900  bg-gradient-to-t ">
             <section class="flex justify-between gap-6 w-8/12 max-w-full m-auto">
             
