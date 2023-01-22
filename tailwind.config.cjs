45d1b5703c31d2c0a4d22808a337a3e3aaed6776
Ylerson Almonte <yacwarefree1@gmail.com>
2023-01-22 16:53:38 -0400

    Add Plugin scroll bar hidden

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
     content: ['./index.html', './main.js', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
         extend: {},
     },
-    plugins: [],
+    plugins: [require('tailwind-scrollbar-hide')],
 };
