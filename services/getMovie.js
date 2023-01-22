7081401206d0ae5aee5343f4b56c2e3d390492d6
Ylerson Almonte <yacwarefree1@gmail.com>
2023-01-22 13:11:14 -0400

    Add functios to get data movie api

---
+++ services/getMovie.js
@@ -0,0 +1,42 @@
+const url = new URL('https://api.themoviedb.org/3/movie/');
+
+export const getMovieDetail = async (id) => {
+    const urlWithId = new URL(id, url);
+    urlWithId.searchParams.set('api_key', import.meta.env.VITE_TMDB_API_KEY);
+    urlWithId.searchParams.set('language', 'es-ES');
+    try {
+        const response = await fetch(urlWithId);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        return error;
+    }
+};
+
+export const getMovieTrailer = async (id) => {
+    const urlWithId = new URL(`${id}/videos`, urlWithId);
+    urlWithId.searchParams.set('api_key', import.meta.env.VITE_TMDB_API_KEY);
+    urlWithId.searchParams.set('language', 'es-ES');
+
+    try {
+        const response = await fetch(urlWithId);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        return error;
+    }
+};
+export const getMovieImage = async (id) => {
+    const urlWithId = new URL(`${id}/images`, url);
+
+    urlWithId.searchParams.set('language', 'es-ES');
+    urlWithId.searchParams.set('api_key', import.meta.env.VITE_TMDB_API_KEY);
+
+    try {
+        const response = await fetch(urlWithId);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        return error;
+    }
+};
